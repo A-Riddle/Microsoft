acdff0754c2d17272d110f35f0a35e525ac75de6
A-Riddle <andrew.riddle.it@gmail.com>
2018-01-07 20:41:05 -0800

    Create Eject-CDdrive.ps1

---
+++ PowerShell/Functions/Misc/More Misc/Eject-CDdrive.ps1
@@ -0,0 +1,8 @@
+function Eject-CDDrive
+{
+    $sh = New-Object -ComObject "Shell.Application"
+    $sh.Namespace(17).Items() |
+    Where-Object { $_.Type -eq "CD Drive" } |
+    foreach { $_.InvokeVerb("Eject") }
+}
+
