3149d0bfc9b17ea10108d3bcb0433ece163164a7
A-Riddle <andrew.riddle.it@gmail.com>
2018-01-09 22:40:12 -0800

    Created PS-Code-Signing.ps1

---
+++ PowerShell/Scripts/Misc/PS-Code-Signing.ps1
@@ -0,0 +1,15 @@
+﻿<#
+Author  :
+Date    : 10/18/2017
+Purpose : Sign Powershell Scripts with a Code Signing Cert from our internal CA.
+#>
+
+# Variables #
+$ScriptPath = "C:\Scripts\Test.ps1"
+$PSCodeCert = (Get-ChildItem Cert:\CurrentUser\My -CodeSigningCert)[0]
+
+# Sign Script #
+Set-AuthenticodeSignature -Certificate $PSCodeCert $ScriptPath
+
+# Check Script Status #
+# Get-AuthenticodeSignature $ScriptPath | ft -AutoSize #
